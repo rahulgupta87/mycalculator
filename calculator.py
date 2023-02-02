cb557a8f291a24ed59cea152cee5f54ddb828fd8
rahulgupta87 <r_gupta87@ymail.com>
2023-02-02 00:35:11 -0500

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,6 +1,10 @@
+import os
+from dotenv import load_dotenv
+import pymongo
 from flask import Flask, request
 app = Flask(__name__)
 
+#### exposing flask api with path /calculate and method type POST
 @app.route("/calculate", methods=["POST"])
 def calculate():
     data = request.get_json()
@@ -15,7 +19,31 @@ def calculate():
         result = operand1 * operand2
     elif operator == "divide":
         result = operand1 / operand2
+
+    #### inserting records in mongodb
+    ## loading monogdb user and password fron env
+    load_dotenv()
+    mongoUser = os.getenv("MONGOUSER")
+    mongoPwd = os.getenv("MONGOPWD")
+    mongoDb = 'dev'
+    mongoCollection = 'calculator-records'
+    
+    ## mongodb client to establish connection with the db cluster
+    mongoClient = pymongo.MongoClient("mongodb+srv://"+mongoUser+":"+mongoPwd+"@cluster-dev.qhs7f.mongodb.net/?retryWrites=true&w=majority")
+    db = mongoClient[mongoDb]
+    collection = db[mongoCollection]
+
+    image = {}
+    image['operator'] = operator
+    image['operand1'] = operand1
+    image['operand2'] = operand2
+    image['result'] = result
+    
+    collection.insert_one(image)
+    print('Data successfully inserted in mongo...')
+
     return {"result": result}
 
 if __name__ == '__main__':
     app.run(debug=True)
+
--- shellProcess.sh
+++ shellProcess.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 
-# Compile the application
+## Compile the application
 echo "Compiling the application..."
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 
-# # Run the tests
-# echo "Running tests..."
-# python -m unittest discover
-
 # Containerize the application
-echo "Containerizing the application..."
+echo "Building the docker image of the application..."
 docker build -t calculator .
 
 # Upload the image to the repository
 echo "Uploading the image to the repository..."
-docker images
 docker tag calculator rgupta87/mycalculator:latest
 docker push rgupta87/mycalculator:latest
 
+# Connect to kubernetes cluster
+echo "Connecting to kubernetes cluster..."
 kubectl config use-context docker-desktop
 
 # Deploy the application to Kubernetes
 echo "Deploying the application to Kubernetes..."
-kubectl apply -f deployment.yaml
+kubectl apply -f deployment_aws.yaml
 
-# Wait for the deployment to complete
+# Restart the deployment
 echo "Waiting for the deployment to complete..."
 kubectl rollout status deployment/calculator
 
